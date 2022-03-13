1d83b0cc928512a92d1b4b445c11b18a6ef58722
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-03-13 13:29:52 -0400

    Button Event Listener Use jQuery to detect when a button is clicked & trigger handler function Store the id of the button that got clicked inside the handler function.

--- game.js
+++ game.js
@@ -19,3 +19,7 @@ $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
 var sound = new Audio("sounds/" + randomChosenColour + ".mp3");
 sound.play();
+
+$("button").click(function (event) {
+    var userChosenColour = $(this).attr("id");
+});
