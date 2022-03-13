a8e4727c7ffb5cfe9bf0b413634b97f78c89f86a
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-03-13 13:43:52 -0400

    Play Sound Function Created a new function play sound to work for both playing sound in, nestSequence function and when a use clicks a buttoon

--- game.js
+++ game.js
@@ -9,21 +9,24 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
-function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-}
 
-var randomChosenColour = buttonColours[randomNumber];
-
-gamePattern.push(randomChosenColour);
-
-$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-var sound = new Audio("sounds/" + randomChosenColour + ".mp3");
-sound.play();
-
-$("button").click(function (event) {
+$("button").click(function () {
     var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
 });
 
-userClickedPattern.push(userChosenColour);
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var sound = new Audio("sounds/" + name + ".mp3");
+    sound.play();
+}
