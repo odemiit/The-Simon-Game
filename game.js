f32af9c89df1715fa0f163c8be116f340b736fac
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-03-13 11:27:45 -0400

    Game Pattern & Random Colour Used the random number generated to select a random color from the array Created an empty array and added the new random chose colour to it.

--- game.js
+++ game.js
@@ -5,6 +5,12 @@ var buttonColours = [
     "yellow"
 ];
 
+var gamePattern = [];
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
 }
+
+var randomChosenColour = buttonColours[randomNumber];
+
+gamePattern.push(randomChosenColour);
