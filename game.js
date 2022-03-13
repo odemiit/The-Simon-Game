cd03b61d4f0686109ee33d06eee066a1f5893871
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-03-13 13:22:19 -0400

    Button Flash and Sound Used jQuery to create a flash animation of button selected. Created a new variable to play the respective sound of chosen colour

--- game.js
+++ game.js
@@ -14,3 +14,8 @@ function nextSequence() {
 var randomChosenColour = buttonColours[randomNumber];
 
 gamePattern.push(randomChosenColour);
+
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+var sound = new Audio("sounds/" + randomChosenColour + ".mp3");
+sound.play();
