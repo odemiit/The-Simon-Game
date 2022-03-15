de8cdc7fc86b01b49aaddcc969fce1600783d6d6
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-03-14 21:37:28 -0400

    Start Over function Created a function to reset the variables when game ends

--- game.js
+++ game.js
@@ -49,6 +49,7 @@ function checkAnswer(currentLevel) {
          }, 200);
 
          $("#level-title").text("Game Over, Press Any Key to Restart");
+         startOver();
     }
 }
 
@@ -82,4 +83,8 @@ function animatePress(currentColour) {
       }, 100);
 }
 
-
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+}
